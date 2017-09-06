1ea15a558ca6bda3390d58604394ef21d95ec585
wadehuber <wadehuber@gmail.com>
2017-09-05 21:29:49 -0700

    Week 3 changes

--- c2/pointer.c
+++ c2/pointer.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-int main () {
-  int num = 3;        /* integer */
-  int* ptr;          /* pointer to integer */
+int main() {
+
+  int num = 6;        /* integer */
+  int* ptr;           /* pointer to integer */
 
   /* & - reference operator - returns the address */
   /* * - dereference operator */
@@ -11,14 +12,19 @@ int main () {
   printf("num=%d, %p, %d\n", num, (void *) &num, sizeof(num));
   printf("ptr=%p, %p, %d, *ptr=%d\n", (void *) ptr, (void *) &ptr, sizeof(ptr), *ptr);
 
-  *ptr = 20;
+
+  /* *ptr is an alias of num */
+  *ptr = 20; 
   printf("\n*ptr = 20\n");
   printf("num=%d, %p, %d\n", num, (void *) &num, sizeof(num));
   printf("ptr=%p, %p, %d, *ptr=%d\n", (void *) ptr, (void *) &ptr, sizeof(ptr), *ptr);
 
   printf("\n");
+  printf("ptr=%p\n", (void *) ptr);
   printf("&(*ptr)=%p\n", (void *) &(*ptr));
   printf("*(&ptr)=%p\n", (void *) *(&ptr));
+  printf("num=%d\n", num);
   printf("*(&num)=%d\n", *(&num));
+
   return 0;
 }
--- c2/strings.c
+++ c2/strings.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  char str1[] = "Hello";
-  char str2[] = {'W', 'o', 'r', 'l', 'd'};
+  char str1[] = "Hello";   /* string (null-terminated character array) */
+  char str2[] = {'W', 'o', 'r', 'l', 'd'}; /* character array (not a string!) */
   int ii;
 
   printf("\nOriginal:\n");
   /* Don't do this */
+  /* str2[x]  ==>  *(str2+x) */
   for(ii=0;ii<15;ii++) {
     printf("str2[%d]=%c (%d)\n", ii, str2[ii], str2[ii]);
   } 
